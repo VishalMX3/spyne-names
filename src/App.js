de824843ed5a5a40c41fee2118478d870ea18749
Vishal Pundhir <f20191055@pilani.bits-pilani.ac.in>
2024-05-27 17:57:17 +0530

    Input form created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+  };
+
   return (
     <div className="App bg-black">
-      <h1 className="text-red-400 text-8xl">React Ap</h1>
+      <form onSubmit={handleSubmit} className="flex space-x-2 mb-4">
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="p-2 border border-gray-300 rounded"
+          placeholder="Enter name"
+        />
+        <button
+          type="submit"
+          className="p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
